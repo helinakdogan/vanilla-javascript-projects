789cb528730abbca00fce8e609165afcad2f9bc0
Helin Akdoğan <akdoganhelin@gmail.com>
2023-03-09 22:16:17 +0300

    09.03.2023

---
+++ text_to_speech_converter/script.js
@@ -0,0 +1,80 @@
+const textarea = document.querySelector("textarea");
+voiceList = document.querySelector("select");
+speechBtn = document.querySelector("button");
+
+let synth = speechSynthesis;
+isSpeaking = true;
+voices();
+
+//to reach different voices
+function voices() {
+    for (let voice of synth.getVoices()) {
+
+        //select "Google US English" voice as default
+        let selected = voice.name === "Google US English" ? "selected" : "";
+
+        //to create an option tag with passing voice name and voice language
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+
+        //to insert option tag beforeend of select tag
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
+
+
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text) {
+    let utterance = new SpeechSynthesisUtterance(text);
+    for (let voice of synth.getVoices()) {
+
+        //if available device voice name is equal to user selected voice name, then set speech voice to user selected voice
+        if (voice.name === voiceList.value) {
+            utterance.voice = voice;
+        }
+    }
+
+    //to hear the speech
+    synth.speak(utterance);
+}
+
+
+
+speechBtn.addEventListener("click", e => {
+    e.preventDefault();
+    if (textarea.value !== "") {
+
+        //if speech is not currently in process on speaking
+        if (!synth.speaking) {
+            textToSpeech(textarea.value);
+        }
+
+        if (textarea.value.length > 80) {
+
+            //checking if speech in speaking process or not in every 100ms
+            //if not then set the value of isSpeaking to true and change the button text
+            setInterval(() => {
+                if (!synth.speaking && !isSpeaking) {
+                    isSpeaking = true;
+                    speechBtn.innerText = "Convert To Speech";
+                } else {
+                }
+            }, 500);
+
+            //if speaking is true, then change its value to false and pause the speech
+            //else change its value to true and resume the speech
+            if (isSpeaking) {
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pause Speech";
+            } else {
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Resume Speech";
+            }
+        } else {
+            speechBtn.innerText = "Convert To Speech";
+        }
+    }
+});
